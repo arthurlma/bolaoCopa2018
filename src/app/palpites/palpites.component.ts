37384519528cf5b55bd980d657393868c95e8055
Arthur Araujo <arthurluiz_araujo@hotmail.com>
2018-03-29 09:03:58 -0300

    palpites inputs, enum and table score
    28/03/18 - inputs of palpites component included, and was started the table scores and make use of enum paises.

--- src/app/palpites/palpites.component.ts
+++ src/app/palpites/palpites.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Paises } from '../paises.enum';
 
 @Component({
   selector: 'app-palpites',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PalpitesComponent implements OnInit {
 
+  iconPais1: string;
+  iconPais2: string;
+  iconPais3: string;
+  iconPais4: string;
+  nomePais1: string;
+  nomePais2: string;
+  nomePais3: string;
+  nomePais4: string;
+
+  @Input() nomeGrupo: string;
+  @Input() set pais1(pais1: string) {
+    this.iconPais1 = '/assets/icons65/' + pais1 + '.png';
+    this.nomePais1 = Paises[pais1];
+  }
+  @Input() set pais2(pais2: string) {
+    this.iconPais2 = '/assets/icons65/' + pais2 + '.png';
+    this.nomePais2 = Paises[pais2];
+  }
+  @Input() set pais3(pais3: string) {
+    this.iconPais3 = '/assets/icons65/' + pais3 + '.png';
+    this.nomePais3 = Paises[pais3];
+  }
+  @Input() set pais4(pais4: string) {
+    this.iconPais4 = '/assets/icons65/' + pais4 + '.png';
+    this.nomePais4 = Paises[pais4];
+  }
+
   constructor() { }
 
   ngOnInit() {
+    // this.pais = Paises['Russia'];
+    // console.log(this.pais);
+    // console.log('this.paises');
+    // console.log(Paises);
   }
+
 
 }
