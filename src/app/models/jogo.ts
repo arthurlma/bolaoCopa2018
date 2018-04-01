ca1276edc403532b56013c51504da14e7705aecf
Arthur Araujo <arthurluiz_araujo@hotmail.com>
2018-04-01 17:11:56 -0300

    easter commit
    finish the live preview.

--- src/app/models/jogo.ts
+++ src/app/models/jogo.ts
@@ -1,10 +1,16 @@
 export class Jogo {
     selecao1: string = null;
     selecao2: string = null;
-    placar1: number = 0;
-    placar2: number = 0;
-    pontosSelecao1: number = 0;
-    pontosSelecao2: number = 0;
+    placar1: number = null;
+    placar2: number = null;
+    pontosSelecao1: number = null;
+    pontosSelecao2: number = null;
+    vitoriaSelecao1: number = null;
+    vitoriaSelecao2: number = null;
+    empateSelecao1: number = null;
+    empateSelecao2: number = null;
+    derrotaSelecao1: number = null;
+    derrotaSelecao2: number = null;
 
 
     constructor(values: Object = {}) {
