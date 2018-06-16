bc71fcf157af0d2d9b163ff6ae0a1a57067b9d31
Arthur Araujo <arthurluiz_araujo@hotmail.com>
2018-06-16 11:01:23 -0300

    commit
    view stackblitz in dev

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyD4aiaPm4_KthOY3SUFX_OhFlernOIj7vM",
+    authDomain: "bolao-copa-18.firebaseapp.com",
+    databaseURL: "https://bolao-copa-18.firebaseio.com",
+    projectId: "bolao-copa-18",
+    storageBucket: "bolao-copa-18.appspot.com",
+    messagingSenderId: "381526423377"
+}
